041deddf1c49088ccd8d1561f4516bc7b6b50daa
Pary Ly <paleepary@aol.com>
2019-12-01 08:39:40 -0800

    change up js. trying a different approach

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,5 @@
+var quizCard = $(".quiz-container")
+
 var quiz = [
     {
         question: "How many teams are there in the NBA?",
@@ -21,12 +23,12 @@ var quiz = [
     },
 ]
 
-var timer = 30;
+
 var intervalId;
 
 var correct = 0;
 var incorrect = 0;
-
+var timer = 30;
 // TIMER
 function start() {
     clearInterval(intervalId);
@@ -50,76 +52,36 @@ function startGame() {
     // LOOPS THROUGH QUESTIONS
     for (var q = 0; q < quiz.length; q++) {
         console.log(quiz[q].question)
-        var quizQuestion = document.createElement('h3');
-        quizQuestion.append(quiz[q].question)
-        document.querySelector('.quiz-container').append(quizQuestion)
+        quizCard.append("<h3>" + quiz[q].question + "</h3>");
         for (var a = 0; a < quiz[q].answer.length; a++) {
-            
-            var quizAnswers = document.createElement("input");
-            quizAnswers.setAttribute("type", "radio");
-            quizAnswers.setAttribute("name", "quiz-");
-            quizAnswers.setAttribute("value", quiz[q].answer[a]);
-            quizAnswers.setAttribute("class", "answerss");
-            var answerBreak = document.createElement('br')
-    
-            document.querySelector('.quiz-container').append(quizAnswers)
-            document.querySelector('.quiz-container').append(quiz[q].answer[a])
-            document.querySelector('.quiz-container').append(answerBreak)
+            quizCard.append("<input type='radio' name='question-" + a + "' value='" + quiz[q].answer[a] + "''>" + quiz[q].answer[a] + "<br>");
         }
     }
 }
 startGame();
 
-
-// $("input").on("click", function (event){
-        
-//         for(var i =0; i < $('input').length; i++){
-//             console.log($('input')[i])
-//             var checker = $('input'+[i]+'[type=radio]:checked').val();
-//             console.log(checker)
-//         }
-        
-//         console.log(quiz.correct)
-//         if(checker === quiz.correct){
-//             console.log(quiz.correct)
-//             correct++;
-//             console.log(correct)
-//     }
-// })
-
-// for (var q = 0; q < quiz.length; q++){
-//     console.log(quiz[q].correct)
-// }
-$("input").on("click", function (event){
-    for (var q = 0; q < quiz.length; q++){
-        // console.log(quiz[q].correct)
-        var checker = document.getElementsByName("quiz-");
-        for (i = 0; i < checker.length; i++){
-            if(checker[i].checked){
-                console.log(checker[i].value) 
-            } 
-        } 
+function done() {
+    $('input').on("click", function(){
+        var inputs = quizCard.children("input:checked");
+    for (var i = 0; i < inputs.length; i++) {
+      if ($(inputs[i]).val() === quiz[i].correct) {
+        console.log($(inputs[i]).val())
+        correct++;
+        console.log(correct)
+      } else {
+        incorrect++;
+        console.log(incorrect)
+      }
     }
+    })
     
-})
-
-
-
-
-
-// function answerChecker(){
+}
+done()
+// $("input").on("click", function (event){
 //     var checker = document.getElementsByName("quiz-");
-//     console.log(checker)
 //     for (i = 0; i < checker.length; i++){
-//         console.log(checker[i].checked)
 //         if(checker[i].checked){
-//             console.log(checker[i].value)
-//         }  
-//     }
-// }
-
-// answerChecker()
-
-
-
-
+//             console.log(checker[i].value) 
+//         } 
+//     } 
+// })
