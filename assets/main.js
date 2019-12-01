4eb37c05c7a6e33d86ccfee21320b1587c8ee888
Pary Ly <paleepary@aol.com>
2019-12-01 07:41:05 -0800

    work on counting the score

--- assets/main.js
+++ assets/main.js
@@ -54,14 +54,14 @@ function startGame() {
         quizQuestion.append(quiz[q].question)
         document.querySelector('.quiz-container').append(quizQuestion)
         for (var a = 0; a < quiz[q].answer.length; a++) {
-            // console.log(quiz[q].answer[a])
+            
             var quizAnswers = document.createElement("input");
             quizAnswers.setAttribute("type", "radio");
-            quizAnswers.setAttribute("name", "quiz-" + q);
+            quizAnswers.setAttribute("name", "quiz-");
             quizAnswers.setAttribute("value", quiz[q].answer[a]);
             quizAnswers.setAttribute("class", "answerss");
             var answerBreak = document.createElement('br')
-
+    
             document.querySelector('.quiz-container').append(quizAnswers)
             document.querySelector('.quiz-container').append(quiz[q].answer[a])
             document.querySelector('.quiz-container').append(answerBreak)
@@ -70,19 +70,56 @@ function startGame() {
 }
 startGame();
 
-var yo = document.getElementsByClassName("answerss").value;
-console.log(yo)
 
-// function done() {
-//     var input = $('input').children("input:checked");
-//     console.log(input)
-//     for (var d = 0; d < input.length; d++) {
-//         if ($(input[i]).val() === quiz[q].correct) {
-//             correct++;
-//             console.log(input)
-//         } else {
-//             incorrect++;
+// $("input").on("click", function (event){
+        
+//         for(var i =0; i < $('input').length; i++){
+//             console.log($('input')[i])
+//             var checker = $('input'+[i]+'[type=radio]:checked').val();
+//             console.log(checker)
 //         }
+        
+//         console.log(quiz.correct)
+//         if(checker === quiz.correct){
+//             console.log(quiz.correct)
+//             correct++;
+//             console.log(correct)
+//     }
+// })
+
+// for (var q = 0; q < quiz.length; q++){
+//     console.log(quiz[q].correct)
+// }
+$("input").on("click", function (event){
+    for (var q = 0; q < quiz.length; q++){
+        // console.log(quiz[q].correct)
+        var checker = document.getElementsByName("quiz-");
+        for (i = 0; i < checker.length; i++){
+            if(checker[i].checked){
+                console.log(checker[i].value) 
+            } 
+        } 
+    }
+    
+})
+
+
+
+
+
+// function answerChecker(){
+//     var checker = document.getElementsByName("quiz-");
+//     console.log(checker)
+//     for (i = 0; i < checker.length; i++){
+//         console.log(checker[i].checked)
+//         if(checker[i].checked){
+//             console.log(checker[i].value)
+//         }  
 //     }
 // }
-// done()
+
+// answerChecker()
+
+
+
+
